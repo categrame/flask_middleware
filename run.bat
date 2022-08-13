1f34bb6df11a08c70f397f797ef2b4aadb7e9083
categrame <gaucher_martin@yahoo.fr>
2022-08-13 18:29:18 +0200

    base flask app with google oauth

---
+++ run.bat
@@ -0,0 +1,8 @@
+set FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
+set FN_BASE_URI=http://localhost:8040
+
+set FLASK_APP=app.py
+set FLASK_DEBUG=1
+set FN_FLASK_SECRET_KEY="the secret key"
+
+python -m flask run -p 8040
